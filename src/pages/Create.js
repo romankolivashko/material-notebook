f6cd4f9b6221581fee9b5b3c9fb11102937f58a3
Roman Kolivashko <rkolivashko@gmail.com>
2022-01-07 20:15:54 -0800

    Added buttons, container style

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,17 +1,32 @@
-import React from 'react'
-import { Typography } from '@material-ui/core';
+import React from "react";
+import { Typography } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
 
 export default function Create() {
   return (
-    <div>
-      <Typography 
+    <Container>
+      <Typography
         variant="h6"
         color="textSecondary"
         component="h2"
         gutterBottom
       >
-        Create a New Note     
+        Create a New Note
       </Typography>
-    </div>
-  )
+      <Button 
+        onClick = {()=> console.log("clicked me")}
+        type="submit"
+        color="secondary"
+        variant="contained"
+      >Submit</Button>
+
+      {/* <Button type="submit" color="secondary" variant="outlined">Un-submit</Button>
+      <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+    </Container>
+  );
 }
