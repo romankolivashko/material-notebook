cec975eff177a7b703ee7d650bf99296b906fae6
Roman Kolivashko <rkolivashko@gmail.com>
2022-01-10 09:51:11 -0800

    Added fetch method and json server

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -13,6 +13,7 @@ import RadioGroup from "@material-ui/core/RadioGroup";
 import { Category } from "@mui/icons-material";
 import FormLabel from "@material-ui/core/FormLabel";
 import FormControl from "@material-ui/core/FormControl";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles({
   field: {
@@ -24,6 +25,7 @@ const useStyles = makeStyles({
 
 export default function Create() {
   const classes = useStyles();
+  const history = useHistory();
   const [title, setTitle] = useState("");
   const [details, setDetails] = useState("");
   const [titleError, setTitleError] = useState(false);
@@ -43,7 +45,11 @@ export default function Create() {
     }
 
     if (title && details) {
-      console.log(title, details, category);
+      fetch('http://localhost:8000/notes', {
+        method: 'POST',
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({ title, details, category })
+      }).then(() => history.push('/'))
     }
   };
 
