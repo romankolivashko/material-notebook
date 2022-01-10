b82f4bc56e01287ad447ba6b3bfcf3f3ba229b90
Roman Kolivashko <rkolivashko@gmail.com>
2022-01-10 10:46:38 -0800

    Added card component and delete method

---
+++ src/components/NoteCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import { IconButton, Typography } from "@material-ui/core";
+import { DeleteOutlined } from "@mui/icons-material";
+import Notes from "../pages/Notes";
+
+export default function NoteCard({ note }) {
+  return (
+    <div>
+      <Card elevation={3}>
+        <CardHeader
+          action={
+            <IconButton onClick={() => console.log('delete', note.title)}>
+              <DeleteOutlined />
+            </IconButton>
+          }
+          title={note.title}
+          subheader={note.category}
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary">
+            {note.details}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
+import NoteCard from "../components/NoteCard";
 
 export default function Notes() {
   const [notes, setNotes] = useState([]);
@@ -29,10 +30,10 @@ export default function Notes() {
         </Grid>
       </Grid> */}
 
-      <Grid container>
+      <Grid container spacing={3}>
         {notes.map((note) => (
           <Grid item key={note.id} xs={12} md={6} lg={4}>
-            <Paper>{ note.title }</Paper>  
+            <NoteCard note={note} />
           </Grid>
         ))}
       </Grid>
